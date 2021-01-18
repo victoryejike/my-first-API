68e53d214a3419dcebe90dd5d71720a5f9240ec0
Ejike <victoryejike@gmail.com>
2021-01-18 01:33:52 +0100

    added express and dotenv packages. created a server with express.js and used dotenv to set environment variables

--- server.js
+++ server.js
@@ -1,55 +1,11 @@
-const http = require('http');
-const { url } = require('inspector');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-    {id: 1, text: 'Todo one'},
-    {id: 2, text: 'Todo two'},
-    {id: 3, text: 'Todo three'},
-]
+//Load ENV file
+dotenv.config({path: './config/config.env'});
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('X-Powered-By', 'Node.Js')
+//Initialize app with express
+const app = express();
 
-    let body = [];
-    const {method, url} = req;
-
-    req.on('data', (chunk)=>{
-        body.push(chunk);
-    }).on('end', ()=>{
-        body = Buffer.concat(body).toString();
-
-        let status = 404;
-        const response = {
-        success: false,
-        data: null,
-    }
-        if(method === 'GET' && url === '/todos'){
-            status = 200;
-            response.success = true;
-            response.data = todos;
-        }else if(method === 'POST' && url === '/todos'){
-            const { id, text } = JSON.parse(body);
-
-            if(!id || !text){
-                status = 400
-            }else {
-                todos.push({ id, text });
-                status = 201;
-                response.success = true;
-                response.data = todos;
-            }
-        }
-        
-        res.writeHead(status, {
-            'Content-Type': 'application/json',
-            'X-Powered-By': 'Node.Js' 
-        })
-        res.end(JSON.stringify(response));
-    })
-    
-})
-
-const PORT = 5000;
-
-server.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`))
