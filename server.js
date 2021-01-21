696438cb26e41aa5466a4d3cf7b46898b77baa1a
Ejike <victoryejike@gmail.com>
2021-01-21 04:04:51 +0100

    separated out our application into different folders such as controllers, routes and middlewares. also added morgan and mongoose packages. Created mongodb atlas database for our app

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
+
+//Route files
+const bootcamps = require('./routes/bootcamps')
 
 //Load ENV file
 dotenv.config({path: './config/config.env'});
 
 //Initialize app with express
 const app = express();
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Mount routers 
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`))
