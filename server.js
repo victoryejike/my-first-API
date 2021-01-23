235752fcf648214876b908571144aef6c88c4192
Ejike <victoryejike@gmail.com>
2021-01-24 00:00:15 +0100

    added bootcamp json data to the application. Created a mongoose database model for storing bootcamps in the database. Added mongoose to app dependencies

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+
+//Load ENV file 192.168.43.178/32 
+dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps')
 
-//Load ENV file
-dotenv.config({path: './config/config.env'});
-
 //Initialize app with express
 const app = express();
+
+//Body parser
+app.use(express.json());
 
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
@@ -22,4 +31,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //close server & exit process
+    server.close(() => process.exit(1));
+})
